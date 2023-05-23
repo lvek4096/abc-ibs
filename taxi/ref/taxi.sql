e2e65486c76a56df26d46030ae18022aee2abc45
lvek4096 <lvek4096@outlook.com>
2023-05-23 18:40:33 +0400

    B3.172

--- taxi/ref/taxi.sql
+++
@@ -1,4 +0,0 @@
-create database taxi;
-use taxi;
-create table taxi_bkgs(bkgid int primary key,start varchar(50),end varchar(50),jdate date,jtime time);
-create table users(uuid varchar(5) primary key,uname varchar(50),passwd varchar(50));
